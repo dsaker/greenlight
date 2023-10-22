cc35be7ec262872891b3c683cf7ff1b100192f55
Dusty Saker <dustysaker@gmail.com>
2023-10-22 14:58:06 -0500

    build linux

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -7,8 +7,8 @@ import (
 	"github.com/felixge/httpsnoop"
 	"github.com/sensor-projekt/greenlight/internal/data"
 	"github.com/sensor-projekt/greenlight/internal/validator"
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -80,20 +80,14 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Only carry out the check if rate limiting is enabled.
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			// Use the realip.FromRequest() function to get the client's real IP address.
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
 			if _, found := clients[ip]; !found {
 				clients[ip] = &client{
-					// Use the requests-per-second and burst values from the config
-					// struct.
 					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
 				}
 			}
